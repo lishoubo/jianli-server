85df4b23cbce352be9f80e5cdf2dcc5a093e12c5
lishoubo <lishoubo@weidian.com>
2016-06-19 15:46:21 +0800

    change requestmapping

--- jl-web/src/main/java/com/jl/platform/web/controller/api/admin/BuildingController.java
+++ jl-web/src/main/java/com/jl/platform/web/controller/api/admin/BuildingController.java
@@ -1,34 +1,29 @@
-/**
- * guanyuhuhu 2016年6月19日
- */
 package com.jl.platform.web.controller.api.admin;
-
-import javax.annotation.Resource;
-import javax.validation.Valid;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.jl.platform.common.Result;
 import com.jl.platform.service.BuildingService;
 import com.jl.platform.service.model.Building;
 import com.jl.platform.web.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
 
 /**
  * @author zhanglu
  */
 @RestController
-@RequestMapping("/api/admin/building")
+@RequestMapping("/api/admin")
 public class BuildingController extends BaseController {
-	@Resource
-	private BuildingService buildingService;
+    @Resource
+    private BuildingService buildingService;
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public Result<String> add(@Valid Building building) {
-		Result<String> result = buildingService.save(building);
-		return result;
-	}
+    @RequestMapping(value = "/building", method = RequestMethod.POST)
+    public Result<String> add(@Valid Building building) {
+        Result<String> result = buildingService.save(building);
+        return result;
+    }
 
 }
