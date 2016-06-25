a2ba00f8cee670d31e8d1f1d3223330869b348a2
zhanglu91 <zlmangostreet@gmail.com>
2016-06-25 13:36:26 +0800

    complete Procedure Enum

--- jl-common/src/main/java/com/jl/platform/common/Procedure.java
+++ jl-common/src/main/java/com/jl/platform/common/Procedure.java
@@ -4,13 +4,21 @@ package com.jl.platform.common;
  * Created by lishoubo on 16/6/18.
  */
 public enum Procedure {
-    PREPPARE("前期准备"),
-    DESIGN("方案设计"),
+	PREPPARE("前期准备"),
 
-    //
-    ;
+	DESIGN("方案设计"),
 
-    Procedure(String desc) {
+	PURCHASE("建材采购"),
 
-    }
+	CONSTRUCTION("硬装施工"),
+
+	ORNAMENT("软装配饰"),
+
+	ACCEPTANCE("工程验收"), ;
+
+	private String desc;
+
+	Procedure(String desc) {
+		this.desc = desc;
+	}
 }
