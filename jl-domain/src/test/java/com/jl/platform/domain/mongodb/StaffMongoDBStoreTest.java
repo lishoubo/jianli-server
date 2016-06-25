db5a0fbd588c3fa84d562df48e4e40b7f98cc23c
zhanglu91 <zlmangostreet@gmail.com>
2016-06-25 13:41:07 +0800

    delete couchdb

--- jl-domain/src/test/java/com/jl/platform/domain/mongodb/StaffMongoDBStoreTest.java
+++ jl-domain/src/test/java/com/jl/platform/domain/mongodb/StaffMongoDBStoreTest.java
@@ -1,49 +1,48 @@
 package com.jl.platform.domain.mongodb;
 
-import com.alibaba.fastjson.JSON;
-import com.jl.platform.service.model.Staff;
+import java.util.Date;
+
 import org.bson.Document;
 import org.junit.Test;
 
-import java.util.Date;
-
-import static org.junit.Assert.*;
+import com.alibaba.fastjson.JSON;
+import com.jl.platform.service.model.Staff;
 
 /**
  * Created by lishoubo on 16/6/24.
  */
 public class StaffMongoDBStoreTest {
 
-    @Test
-    public void test_modal() throws Exception {
-        System.out.println(19 / 10);
+	@Test
+	public void test_modal() throws Exception {
+		System.out.println(19 / 10);
 
-    }
+	}
 
-    @Test
-    public void test_bench() throws Exception {
-        Staff staff = new Staff();
-        staff.setType("type");
-        staff.setName("name");
-        staff.setGrade("grade");
-        staff.setProfessional("profefe");
-        staff.setQualification("quqfefe");
-        staff.setCreateTime(new Date());
+	@Test
+	public void test_bench() throws Exception {
+		Staff staff = new Staff();
+		staff.setType("type");
+		staff.setName("name");
+		staff.setGrade("grade");
+		staff.setProfessional("profefe");
+		staff.setQualification("quqfefe");
+		staff.setCreateDate(new Date());
 
-        int times = 10000;
-        long begin = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            String json = JSON.toJSONString(staff);
-            Document document = new Document();
-            document.putAll(JSON.parseObject(json));
-        }
-        System.out.println("fastjson:" + (System.currentTimeMillis() - begin));
+		int times = 10000;
+		long begin = System.currentTimeMillis();
+		for (int i = 0; i < times; i++) {
+			String json = JSON.toJSONString(staff);
+			Document document = new Document();
+			document.putAll(JSON.parseObject(json));
+		}
+		System.out.println("fastjson:" + (System.currentTimeMillis() - begin));
 
-
-        begin = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            Document document = new Document(Document.parse(JSON.toJSONString(staff)));
-        }
-        System.out.println("Document:" + (System.currentTimeMillis() - begin));
-    }
+		begin = System.currentTimeMillis();
+		for (int i = 0; i < times; i++) {
+			Document document = new Document(Document.parse(JSON
+					.toJSONString(staff)));
+		}
+		System.out.println("Document:" + (System.currentTimeMillis() - begin));
+	}
 }
